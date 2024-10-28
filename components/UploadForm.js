a797ed119ca6b4ea342c91d3699eac4e59204c58
yasermofatteh <116434707+yasermofatteh@users.noreply.github.com>
2024-10-29 00:42:45 +0100

    UploadForm.js

---
+++ components/UploadForm.js
@@ -0,0 +1,31 @@
+// components/UploadForm.js
+import { useState } from 'react';
+import { uploadToIPFS } from '../utils/ipfs';
+import { storeModelHash } from '../utils/blockchain';
+
+function UploadForm() {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleUpload = async () => {
+        if (file) {
+            const hash = await uploadToIPFS(file);
+            await storeModelHash(hash);
+            alert("Model uploaded successfully!");
+        } else {
+            alert("Please select a file first.");
+        }
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload & Save</button>
+        </div>
+    );
+}
+
+export default UploadForm;
