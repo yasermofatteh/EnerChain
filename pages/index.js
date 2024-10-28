dd0bfd1178fb33fbc13ed1bccf26e0306a2ab8f1
yasermofatteh <116434707+yasermofatteh@users.noreply.github.com>
2024-10-29 00:41:14 +0100

    index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import UploadForm from '../components/UploadForm';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>EnerChain Knowledge Model Upload</h1>
+            <UploadForm />
+        </div>
+    );
+}
